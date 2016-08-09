1f8a0298ca9a81b2a89c5e612c926755d601c464
zhoulin <zhoulin@zhoulin900.local>
2016-08-09 13:30:45 +0800

    开始开发回复页

---
+++ app/Pages/ReponsePage.component.js
@@ -0,0 +1,16 @@
+"use strict";
+var core_1 = require('@angular/core');
+var ResponsePage = (function () {
+    function ResponsePage() {
+    }
+    ResponsePage = __decorate([
+        core_1.Component({
+            selector: 'response-page',
+            template: "\n  ",
+        }), 
+        __metadata('design:paramtypes', [])
+    ], ResponsePage);
+    return ResponsePage;
+}());
+exports.ResponsePage = ResponsePage;
+//# sourceMappingURL=ReponsePage.component.js.map
---
+++ app/Pages/ReponsePage.component.ts
@@ -0,0 +1,15 @@
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
+
+@Component({
+  selector:'response-page',
+  template:`
+  `,
+})
+export class ResponsePage {
+  constructor() {
+  }
+
+}
